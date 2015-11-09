fb8c196843c45627e77c31e376d7278c9caac763
Vivek Supe <viveksupe07@gmail.com>
2015-11-08 19:02:44 -0500

    Added Tips Model

---
+++ src/main/java/models/Tip.java
@@ -0,0 +1,63 @@
+package models;
+
+import java.util.Date;
+
+/**
+ * Created by vivek on 11/8/15.
+ */
+public class Tip {
+    private String user_id;
+    private String text;
+    private String business_id;
+    private double likes;
+    private Date date;
+    private String type;
+
+    public String getUser_id() {
+        return user_id;
+    }
+
+    public void setUser_id(String user_id) {
+        this.user_id = user_id;
+    }
+
+    public String getText() {
+        return text;
+    }
+
+    public void setText(String text) {
+        this.text = text;
+    }
+
+    public String getBusiness_id() {
+        return business_id;
+    }
+
+    public void setBusiness_id(String business_id) {
+        this.business_id = business_id;
+    }
+
+    public double getLikes() {
+        return likes;
+    }
+
+    public void setLikes(double likes) {
+        this.likes = likes;
+    }
+
+    public Date getDate() {
+        return date;
+    }
+
+    public void setDate(Date date) {
+        this.date = date;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+}
